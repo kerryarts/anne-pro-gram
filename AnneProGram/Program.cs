af3c104fce71f5e802d3205eb7a87263ab6d3fe3
Kerry <kerryarts@users.noreply.github.com>
2018-07-06 22:45:04 +1200

    Add firmware command to app

--- AnneProGram/Program.cs
+++ AnneProGram/Program.cs
@@ -1,15 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AnneProGram.Firmware;
+using System;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnneProGram
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static int Main(string[] args)
 		{
+			if (args.Length == 0)
+				return ShowUsage();
+
+			switch (args[0])
+			{
+				case "-fw" when (args.Length == 2):
+					return Firmware(args[1]);
+
+				default:
+					return ShowUsage();
+			}
+		}
+
+		private static int Firmware(string inputFilePath)
+		{
+			try
+			{
+				var inputBytes = File.ReadAllBytes(inputFilePath);
+				var editor = new FirmwareEditor(inputBytes);
+				var mods = FirmwareMods.Values;
+
+				foreach (var mod in mods)
+					editor.ApplyMod(mod);
+
+				var outputBytes = editor.GetBytes();
+
+				var inputFolder = Path.GetDirectoryName(inputFilePath);
+				var inputFileName = Path.GetFileNameWithoutExtension(inputFilePath);
+				var inputFileExtension = Path.GetExtension(inputFilePath);
+				var outputFileName = $"{inputFileName}_{string.Join("_", mods.Select(m => m.Name))}{inputFileExtension}";
+				var outputFilePath = Path.Combine(inputFolder, outputFileName);
+
+				File.WriteAllBytes(outputFilePath, outputBytes);
+
+				Console.WriteLine("Done.");
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine(ex);
+				return -1;
+			}
+		}
+
+		private static int ShowUsage()
+		{
+			Console.WriteLine("Usage:");
+			Console.WriteLine("\t-fw inputFilePath");
+			return -1;
 		}
 	}
 }
